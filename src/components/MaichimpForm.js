44e920fa7a739faf1a1cad7ed1b6fa6822728c4c
Jinuka <sethjinuka@gmail.com>
2025-04-05 02:18:45 +0530

    firstCommit

---
+++ src/components/MaichimpForm.js
@@ -0,0 +1,20 @@
+import MailchimpSubscribe from "react-mailchimp-subscribe";
+import { Newsletter } from "./Newsletter";
+
+export const MaichimpForm = () => {
+    const postUrl = `$(process.env.MAILCHIMP_URL)?u=$(process.env.MAILCHIMP_U)&id=$(process.env.MAILCHIMP_ID)`;
+    return (
+        <>
+            <MailchimpSubscribe
+                url={postUrl}
+                render={({ subscribe, status, message }) => (
+                    <Newsletter
+                    status={status}
+                    message={message}
+                    onValidated={(formData) => subscribe(formData)} 
+                    />
+                )}
+            />
+        </>
+    )
+}
---
+++ src/components/Newsletter.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { Alert, Col, Row } from "react-bootstrap";
+export const Newsletter = ({subscribe, status, message}) => {
+
+    const [email, setEmail] = useState("");
+
+    useEffect(() => {
+        if (status === "success") clearFields();
+    }, [status])
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        email &&
+        email.indexOf("@") > -1 &&
+        onValidated9({
+            EMAIL: email
+        })
+    }
+
+    const clearFields = () => {
+        setEmail("");
+    }
+
+    return (
+        <Col lg={12}>
+            <div className="newsletter-bx">
+                <Row>
+                    <Col lg={12} md={6} xl={5}>
+                        <h3>Subscribe to my newsletter</h3>
+                        <p>Get notified when I publish something new, right in your inbox.</p>
+                        {status === "sending" && <Alert>Sending...</Alert>}
+                        {status === "error" && <Alert variant="danger">{message}</Alert>}
+                        {status === "success" && <Alert variant="success">{message}</Alert>}
+                    </Col>
+                    <Col md={6} xl={7}>
+                        <form onSubmit={subscribe}>
+                            <div className="new-email-bx">
+                                <input value={status === "success" ? "" : status === "error" ? "" : email} type="email" placeholder="Email Address" onChange={(e) => setEmail(e.target.value)}/>
+                                <button type="submit">Subscribe</button>
+                            </div>
+                        </form>
+                    </Col>
+                </Row>
+            </div>
+        </Col>
+    )
+}
